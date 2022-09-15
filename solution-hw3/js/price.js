8f2f6702ed3e32332c064b28d176439d7236c62c
lzx0406 <zexuanli@andrew.cmu.edu>
2022-09-15 14:40:59 -0400

    init hw3

---
+++ solution-hw3/js/price.js
@@ -0,0 +1,6 @@
+function glazingChange(element) {
+    // get value of selected glazing option
+    const priceChange = element.value;
+  // add your code to do update the price ...
+  //(basePrice + glazingPrice) * packPrice
+  }
