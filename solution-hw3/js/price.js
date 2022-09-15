5c4dc0d6889000eea023793f0841988e146f6e1b
lzx0406 <zexuanli@andrew.cmu.edu>
2022-09-15 19:16:14 -0400

    hw3 v1

--- solution-hw3/js/price.js
+++ solution-hw3/js/price.js
@@ -1,6 +1,26 @@
+let current = {
+  price: Number(0),
+  base: Number(2.49),
+  glazing: Number(0),
+  pack: Number(1)
+};
+
 function glazingChange(element) {
-    // get value of selected glazing option
-    const priceChange = element.value;
-  // add your code to do update the price ...
-  //(basePrice + glazingPrice) * packPrice
+  const glazeChange = element.value;
+  current.glazing = glazeChange;
+
+  current.element = document.querySelector('#item_price');
+  const newP = (parseFloat(current.base) + parseFloat(current.glazing)) * parseFloat(current.pack);
+  console.log("new price is " + newP);
+  current.element.innerText = "$" + newP.toFixed(2); 
+  }
+
+function packChange(element) {
+  const packChange = element.value;
+  current.pack = packChange;
+
+  current.element = document.querySelector('#item_price');
+  const newP = (parseFloat(current.base) + parseFloat(current.glazing)) * parseFloat(current.pack);
+  console.log("new price is " + newP);
+  current.element.innerText = "$" + newP.toFixed(2); 
   }
