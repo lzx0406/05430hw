765107e714a06a4d235a8a5948d37b1833d1866e
lzx0406 <zexuanli@andrew.cmu.edu>
2022-09-22 01:48:43 -0400

    revised dropdown

--- solution-hw3/js/price.js
+++ solution-hw3/js/price.js
@@ -1,5 +1,46 @@
+// Initializing the dropdown
+class Glazing {
+  value;
+  text;
+  constructor(value, text) {
+    this.value = value;
+    this.text = text;
+  }
+}
+let allGlazing = [new Glazing(0,"Keep Original"), new Glazing(0,"Sugar milk"), new Glazing(0.5,"Vanilla milk"), new Glazing(1.5,"Double chocolate")];
+
+class Pack{
+  value;
+  text;
+  constructor(value, text) {
+    this.value = value;
+    this.text = text;
+  }
+}
+let allPack = [new Pack(1, '1'), new Pack(3, '3'), new Pack(5, '6'), new Pack(10, '12')];
+
+let selectG = document.querySelector('#glazingOptions');
+let selectP = document.querySelector('#packOptions');
+
+for(var i = 0; i < allGlazing.length; i++) {
+  var opt = allGlazing[i];
+  var newop = document.createElement('option');
+  newop.textContent = opt.text;
+  newop.value = opt.value;
+  selectG.appendChild(newop);
+}
+
+for(var i = 0; i < allPack.length; i++) {
+  var opt = allPack[i];
+  var newop = document.createElement('option');
+  newop.textContent = opt.text;
+  newop.value = opt.value;
+  selectP.appendChild(newop);
+}
+
+
+// All other functions calculating new cost
 let current = {
-  price: Number(0),
   base: Number(2.49),
   glazing: Number(0),
   pack: Number(1)
