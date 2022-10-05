1b88ef2e77918f2f33bfa184ff7dc56e309a5647
lzx0406 <zexuanli@andrew.cmu.edu>
2022-10-05 15:35:22 -0400

    finished but haven't checked

--- solution-hw5/js/price.js
+++ solution-hw5/js/price.js
@@ -3,20 +3,27 @@ const queryString = window.location.search;
 console.log(queryString);
 const params = new URLSearchParams(queryString);
 console.log(params);
-const rollType = params.get('roll');
+let rollType = params.get('roll');
 console.log(rollType);
 
 // Initialize the image
 const rollImage = document.querySelector('#roll-detail-img');
-rollImage.src = 'assets/products/' + rolls[rollType].imageFile;
+if (rollType != null){
+  rollImage.src = 'assets/products/' + rolls[rollType].imageFile;
+}
 
 // Initialize the title
 const detailTitle = document.querySelector('#detail-topic');
-detailTitle.innerText = rollType + ' Cinnamon Roll';
+if (rollType != null){
+  detailTitle.innerText = rollType + ' Cinnamon Roll';
+}
+
 
 // Initialize the price
 const detailBasePrice = document.querySelector('#item_price');
-detailBasePrice.innerText = "$" + rolls[rollType].basePrice;
+if (rollType != null){
+  detailBasePrice.innerText = "$" + rolls[rollType].basePrice;
+}
 
 
 // Initializing the dropdown
@@ -43,7 +50,8 @@ let allPack = [new Pack(1, '1'), new Pack(3, '3'), new Pack(5, '6'), new Pack(10
 let selectG = document.querySelector('#glazingOptions');
 let selectP = document.querySelector('#packOptions');
 
-for(var i = 0; i < allGlazing.length; i++) {
+if (rollType != null){
+  for(var i = 0; i < allGlazing.length; i++) {
   var opt = allGlazing[i];
   var newop = document.createElement('option');
   newop.textContent = opt.text;
@@ -58,9 +66,12 @@ for(var i = 0; i < allPack.length; i++) {
   newop.value = opt.value;
   selectP.appendChild(newop);
 }
-
+}
 
 // All other functions calculating new cost
+if (rollType == null){
+  rollType = "Original";
+}
 let current = {
   base: Number(rolls[rollType].basePrice),
   glazing: Number(0),
@@ -116,3 +127,85 @@ function addToCart(){
   cart.push(newItem);
   console.log(cart);
 }
+
+let curCart = new Set();
+let initApple = new Roll("Apple", 
+    allGlazing[0], 
+    allPack[1],
+    rolls["Apple"].basePrice);
+curCart.add(initApple);
+
+let initRaisin = new Roll("Raisin", 
+    allGlazing[1], 
+    allPack[1],
+    rolls["Raisin"].basePrice);
+curCart.add(initRaisin);
+
+let initWalnut = new Roll("Walnut", 
+    allGlazing[2], 
+    allPack[3],
+    rolls["Walnut"].basePrice);
+curCart.add(initWalnut);
+
+let initOriginal = new Roll("Original", 
+    allGlazing[1], 
+    allPack[0],
+    rolls["Original"].basePrice);
+curCart.add(initOriginal);
+
+function initCart(newRoll){
+  let template = document.querySelector("#var-cart");
+  if (template != null) {
+    let clone = template.content.cloneNode(true);
+    let element = clone.querySelector(".cart-line");
+    document.querySelector('.cart').prepend(element); 
+
+    let deleteButton = element.querySelector('.remove');
+    deleteButton.addEventListener('click', (event) => removeItem(event, newRoll, element));
+
+    //Init the image, text and stuff for each cart item
+    let CartImageElement = element.querySelector('#cart_img_id');
+    CartImageElement.src = 'assets/products/' + rolls[newRoll.type].imageFile;
+
+    let CartItemName = element.querySelector('#cart-item-name');
+    CartItemName.innerText = newRoll.type + " Cinnamon Roll";
+
+    let CartGlazing = element.querySelector('#cart-glazing');
+    CartGlazing.innerText = "Glazing: " + newRoll.glazing.text;
+
+    let CartPack = element.querySelector('#cart-pack-size');
+    CartPack.innerText = "Pack size: " + newRoll.size.text;
+
+    const newP = (parseFloat(newRoll.basePrice) + parseFloat(newRoll.glazing.value)) * parseFloat(newRoll.size.value);
+    let CartPrice = element.querySelector('#cart-item-price');
+    CartPrice.innerText = "$" + newP.toFixed(2);
+  }
+}
+
+function updateTotalPrice(){
+  let totalPrice = 0;
+  for (let item of curCart){
+    const oneP = (parseFloat(item.basePrice) + parseFloat(item.glazing.value)) * parseFloat(item.size.value);
+    totalPrice = totalPrice + oneP;
+    let CartTotalPrice = document.querySelector('#total2');
+    if (CartTotalPrice != null){
+      CartTotalPrice.innerText = "$" + totalPrice.toFixed(2);
+    }
+  }
+  
+}
+
+function removeItem(event, item, element){
+  event.preventDefault();
+  if (curCart.size > 0){
+    element.remove();
+    curCart.delete(item);
+  }
+  console.log(curCart);
+  updateTotalPrice();
+}
+
+for (let item of curCart) {
+  initCart(item);
+}
+updateTotalPrice();
